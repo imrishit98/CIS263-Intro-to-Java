72b29503cca628cd9a01aafc2096ad77d729cd15
Rishit Patel <49016509+imrishit98@users.noreply.github.com>
2020-02-15 00:46:31 -0500

    Add files via upload

---
+++ BasicMethods/BasicMethods.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * @author Rishit Patel
+ * @date Jan. 8, 2020
+ * Description:
+ */
+public class BasicMethods {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+    public static double anotherMethod(){
+        double result = 0;
+        return result;
+    }
+}
